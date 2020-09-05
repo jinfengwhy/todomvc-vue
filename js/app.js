785c4f15f4a9d6b03cec8188d0ab2eda71cc9614
xingzhe <xingzhe290@foxmail.com>
2020-09-05 12:01:41 +0800

    添加任务

--- js/app.js
+++ js/app.js
@@ -22,6 +22,27 @@
 		data: {
 			// 下面写法等同于 todos: todos 属于 ES 6 简写方式
 			todos
+		},
+		methods: {
+			handleNewTodoKeyDown (e) {
+				// 0. 注册 enter 键按下的事件
+				// 1. 获取文本框的值
+				// 2. 数据校验
+				// 3. 添加到 todos 
+				// 4. 清空文本框
+				const target = e.target
+				const value = target.value
+				if (!value.length) {
+					return
+				}
+				const todos = this.todos
+				todos.push({
+					id: todos[todos.length - 1] + 1,
+					title: value,
+					completed: false
+				})
+				target.value = ''
+			}
 		}
 	}).$mount('#app')
 })()
