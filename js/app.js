5a62b0eed6949512f8dfae79583e9e70b686b87b
xingzhe <xingzhe290@foxmail.com>
2020-09-06 14:11:09 +0800

    列表渲染（父子组件通信-Props Down）
    1. 在父组件中通过子组件标签声明属性的方式传递数据
    2. 在子组件中声明 props 接收父组件传递给自己的数据

--- js/app.js
+++ js/app.js
@@ -1,11 +1,29 @@
 ;(function () {
 
+	const todos = [
+		{
+			id: 1,
+			title: '吃饭',
+			completed: true
+		},
+		{
+			id: 2,
+			title: '写代码',
+			completed: false
+		},
+		{
+			id: 3,
+			title: '溜达',
+			completed: true
+		},
+	]
+
 	// 这里添加了 id 属性，是为了方便进行 dom 操作
 	const template = `
 		<div id="app">
 			<section class="todoapp">
 				<todo-header></todo-header>
-				<todo-list></todo-list>
+				<todo-list :list="todos"></todo-list>
 				<todo-footer></todo-footer>
 			</section>
 			<app-footer></app-footer>
@@ -13,6 +31,11 @@
 	`
 
 	window.App = {
+		data () {
+			return {
+				todos
+			}
+		},
 		template,
 		components: {
 			// ES 6 简写方式
